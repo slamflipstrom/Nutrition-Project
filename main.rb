f0a9b262c4daa5a5dc58b15a73ca04555f0e2fc1
slamflipstrom <lindstrom.s@gmail.com>
2014-08-20 09:33:22 -0500

    Project Change
    food_info API appears to be broken. Refactoring as a simple recipe
    storing application,

--- main.rb
+++ main.rb
@@ -8,15 +8,35 @@ require "sinatra/activerecord"
 
 require "sqlite3"
 
-FoodInfo.establish_connection(:fat_secret, :key => '024cc6920ca54c408e7ded2bde7a84ec', :secret => '32daeea219e44e81836a7a09d7a66c57')
+# FoodInfo.establish_connection(:fat_secret, :key => '024cc6920ca54c408e7ded2bde7a84ec', :secret => '32daeea219e44e81836a7a09d7a66c57')
 
 require_relative "./models/category.rb"
 require_relative "./models/food.rb"
 require_relative "./models/recipe.rb"
 
 get "/" do
+  @allcategories = Category.all
   
   erb :home
 end
+
+get "/recipes/:category_id" do
+  c = Category.find(params[:category_id])
+  @food = c.recipes
+  
+  erb :recipe
+end
+
+get "/add_recipe" do
+ 
+ erb :add
+end
+
+get "/do_stuff" do
+  Recipe.create(recipe_name: params["new_recipe"])
+  
+  erb :category
+end
+
 
 binding.pry 
--- models/food.rb
+++ models/food.rb
@@ -1,10 +1,11 @@
-class Foods < ActiveRecord::Base
+class Food < ActiveRecord::Base
   has_and_belongs_to_many :recipes
-  
+
+  # def self.populate
+  #   allfoods = Foods.all
+  #   foods << allfoods
+
+  # end
+    
 end
 
-def self_populate
-  allfoods = Foods.all
-  foods << allfoods
-
-end
